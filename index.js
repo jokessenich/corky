c6cc5e43f088476b30c9c328d1b8e8f100f61a6c
jokessenich <johnny.kessenich@gmail.com>
2020-02-25 21:26:16 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function handleHome(){
+    let header = document.getElementById('hero')
+    let phrase = `"My awareness was at a pinpoint. There was no thinking, no experiencing; there was no nothing. There was me in the light as part of the light and no thought."`
+    header.innerHTML = phrase
+}
+
+$(handleHome())

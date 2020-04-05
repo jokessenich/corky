ebadcdb780a08413e25aabd1b0ef5aa35e4abb65
jokessenich <johnny.kessenich@gmail.com>
2020-04-05 13:31:35 -0700

    Early version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function handleHome(){
-    let header = document.getElementById('hero')
+    let header = document.getElementById('quote')
     let phrase = `"My awareness was at a pinpoint. There was no thinking, no experiencing; there was no nothing. There was me in the light as part of the light and no thought."`
     header.innerHTML = phrase
 }
